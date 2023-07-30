09e23cd3089d1405e79ea8ef32a027feed4f6fde
Carlos Costoso <carlos.costoso@outlook.com>
2023-07-30 12:49:30 -0400

    Adds Listing controller

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ListingModel extends Model
+class Listing extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'];
 }
