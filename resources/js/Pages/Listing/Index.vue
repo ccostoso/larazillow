f97dc678398bd7111dd1887d71d901eb7919af32
Carlos Costoso <carlos.costoso@outlook.com>
2023-07-31 18:57:19 -0400

    Adds edit and delete resource routes and installs tailwind

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,19 +1,19 @@
 <template>
   <div v-for="listing in listings" :key="listing.id">
     <div>
-      <Link :href="`/listing/${listing.id}`">
+      <Link :href="route('listing.show', {listing: listing.id})">
         <ListingAddress :listing="listing" />
       </Link>
     </div>
 
     <div>
-      <Link :href="`/listing/${listing.id}/edit`">
+      <Link :href="route('listing.edit', {listing: listing.id})">
         Edit
       </Link>
     </div>
 
     <div>
-      <Link :href="`/listing/${listing.id}`" method="DELETE" as="button">
+      <Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE" as="button">
         Delete
       </Link>
     </div>
